e08de8e06946d446232153cb4ee43211192a3a1d
Wooogy <hswook12@me.com>
2022-10-18 21:58:36 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,31 +1,50 @@
+# def solution(s):
+#     number_hash = {
+#         "zero": "0",
+#         "one": "1",
+#         "two": "2",
+#         "three": "3",
+#         "four": "4",
+#         "five": "5",
+#         "six": "6",
+#         "seven": "7",
+#         "eight": "8",
+#         "nine": "9",
+#     }
+#     answer = ""
+#     for i in range(len(s)):
+#         if s[i].isdigit():
+#             answer += s[i]
+#         else:
+#             if s[i:i+3] in number_hash:
+#                 answer += number_hash[s[i:i+3]]
+#                 i += 3
+#                 continue
+#             if s[i:i+4] in number_hash:
+#                 answer += number_hash[s[i:i+4]]
+#                 i += 4
+#                 continue
+#             if s[i:i+5] in number_hash:
+#                 answer += number_hash[s[i:i+5]]
+#                 i += 5
+#                 continue
+#     return int(answer)
+
+number_hash = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
 def solution(s):
-    number_hash = {
-        "zero": "0",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
-    answer = ""
-    for i in range(len(s)):
-        if s[i].isdigit():
-            answer += s[i]
-        else:
-            if s[i:i+3] in number_hash:
-                answer += number_hash[s[i:i+3]]
-                i += 3
-                continue
-            if s[i:i+4] in number_hash:
-                answer += number_hash[s[i:i+4]]
-                i += 4
-                continue
-            if s[i:i+5] in number_hash:
-                answer += number_hash[s[i:i+5]]
-                i += 5
-                continue
+    answer = s
+    for key, value in number_hash.items():
+        answer = answer.replace(key, value)
     return int(answer)
