2333354697f95fcb4628737c0e7abe67a70e47aa
Wooogy <hswook12@me.com>
2022-10-18 21:58:29 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,2 +1,6 @@
 def solution(array, commands):
     return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]
+
+
+# def solution(array, commands):
+#     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
