76cc2ec9378618183d08c32aa005d5084878b2b5
Wooogy <hswook12@me.com>
2022-12-13 03:37:35 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,11 +1,12 @@
 def solution(k, m, score):
     answer = 0
     score.sort(reverse=True)
-    for i in range(0 , len(score), m):
+    for i in range(0 , len(score) - len(score) % m, m):
         box = score[i:i+m]
+        answer += box[m-1] * m
         
-        if len(box) == m:
-            answer += box[m-1] * m
+        # if len(box) == m:
+        #     answer += box[m-1] * m
             
     # while len(score) >= m:
     #     answer += score[:m][m-1] * m
