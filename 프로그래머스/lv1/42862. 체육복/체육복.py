9cc516dca4055f826b0b89785a1866072ca4b485
Wooogy <hswook12@me.com>
2022-12-18 03:39:01 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,9 @@
+def solution(n, lost, reserve):
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
+    for r in set_reserve:
+        if r-1 in set_lost:
+            set_lost.remove(r-1)
+        elif r+1 in set_lost:
+            set_lost.remove(r+1)
+    return n - len(set_lost)
