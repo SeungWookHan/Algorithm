a219072ca0e527db1a4f7fb155198b9874897cdb
Wooogy <hswook12@me.com>
2023-05-03 01:04:44 +0900

    Time: 56 ms (5.10%), Space: 16.3 MB (5.05%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -6,15 +6,14 @@ class Solution:
         if n < 2:
             return n
         left = 1
-        right = n #5
-        while left <= n:
-            mid = (left + right) // 2
-            if isBadVersion(mid) and not isBadVersion(mid-1):
-                return mid
-            elif isBadVersion(mid-1):
-                right = mid-1
+        right = n 
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
             else:
-                left = mid+1
+                left = mid + 1
+        return left
                 
         
                 
