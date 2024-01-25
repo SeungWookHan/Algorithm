74358df11a832150c7152bd3d39e2e4f4694b72f
Wooogy <seungwook.han.dev@gmail.com>
2024-01-25 12:58:44 +0900

    Time: 41 ms (66.14%), Space: 16.7 MB (54.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,23 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = int(m) - 1
-        j = int(n) - 1
-        k = int(m + n) - 1
-        
-        while k >= 0:
-            if i >= 0 and j >= 0:
-                if(nums2[j] > nums1[i]):
-                    nums1[k] = nums2[j]
-                    k -= 1
-                    j -= 1
-                else:
-                    nums1[k] = nums1[i]
-                    k -= 1
-                    i -= 1
-            elif j >= 0:
-                nums1[k] = nums2[j]
-                k -= 1
-                j -= 1
-            else:
-                break
+        if len(nums1) != m+n:
+            return
+        nums1[m:] = nums2
+        nums1.sort()
